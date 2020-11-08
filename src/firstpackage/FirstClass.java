8322f89b2a2301a3b3e1a1f11aac5094eb7030ef
JohnQ1981 <69947888+JohnQ1981@users.noreply.github.com>
2020-11-08 10:25:28 -0600

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,6 +8,9 @@ public class FirstClass {
 		System.out.println("Second commit from local master branch");
 		System.out.println("Third commit from local Master Branch");
 		System.out.println("Fourth commit from remote Master Branch");
+		List<String> list = new ArrayList<>();
+		list.add("J");
+		list.add("T");
 		for (int i=0; i<3;i++){
 			System.out.println("From Git hub hello");
 		}
