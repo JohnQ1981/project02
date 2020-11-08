e671525dacc2428428c1582a46f6da41d800029c
John Q <ikram1981@gmail.com>
2020-11-08 14:34:53 -0600

    T

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -2,6 +2,7 @@ package firstpackage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FirstClass {
 
@@ -11,7 +12,7 @@ public class FirstClass {
 		System.out.println("Second commit from local master branch");
 		System.out.println("Third commit from local Master Branch");
 		System.out.println("Fourth commit from remote Master Branch");
-		List<String> list = new ArrayList<>();
+		List<String> list1 = new ArrayList<>();
 		list.add("J");
 		list.add("T");
 		list.add("G");
@@ -45,8 +46,15 @@ public class FirstClass {
 
 		System.out.println("This will be conflict from GitHub");
 
-
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the how many elements you want to have in list");
+		int a = scan.nextInt();
+		List<String> list = new ArrayList<>();
+		for(int i =0 ; i<a; i++) {
+			System.out.println("Now Enter Strings for the list");
+			list.add(scan.nextLine());
+		}
+		System.out.println("Your list is "+ list);
 	}
 
 }
